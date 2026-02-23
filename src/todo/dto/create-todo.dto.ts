f0fde2abc72a43d067dd8469ea56e8eee0b763df
Alex <fornikaras7@gmail.com>
2026-02-23 21:20:08 +0500

    feat: add validation

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,4 +1,10 @@
+import { IsString, Length } from 'class-validator';
+
 export class CreateTodoDTO {
+  @IsString({ message: 'Заголовок должен быть строкой' })
+  @Length(2, 10)
   title: string;
+  @IsString()
+  @Length(3, 50)
   description: string;
 }
